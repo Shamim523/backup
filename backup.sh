e77b768c6a9655469a866886c07da80a8a71c640
Shamim <Shamim003@gmail.com>
2024-07-09 05:26:17 -0400

    [+] This is first time i try to make script in bash

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Author : Shamim Hossain
+# Date of Create : 09/07/2024
+# Last modified : 09/07/2024
+
+# Discription 
+# [+] This script take back up your Home directory on Current working directory
+
+# Usage 
+# [+] To run this script just simply write bash and script name [ ex: bash backup.sh]
+
+tar -cvf ~/git/backup-"$(date +%d.%m.%Y-%S.%M.%H)".tar ~/* 2> /dev/null
+exit 0
